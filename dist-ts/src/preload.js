aa2bb1e25789fd939502769772c9155bb187847f
AllTools Development <dev@alltools.local>
2026-08-18 15:26:47 +0000

    chore: ignore generated TypeScript output

--- dist-ts/src/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('alltools', {
-    catalog: {
-        list: () => electron_1.ipcRenderer.invoke('catalog:list')
-    }
-});
-//# sourceMappingURL=preload.js.map
